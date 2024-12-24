87b9aeb03a09ed334f3064497980e6674ff5762e
ZloyAdmin <34090819+ZloyAdmin@users.noreply.github.com>
2024-12-24 17:40:29 +0300

    Update build.groovy

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,17 +1,16 @@
 void call(){
-  stage("a"){
-    println("build maven project")
-  }
-  stage("b"){
-    println("bbbb")
-  }
-  stage("c"){
-    println("cccc")
-  }
-  stage("d"){
-
-    node { label 'slave'}
-
-    sh "ls -l && hostname"
+    node('slave'){
+      stage("a"){
+        println("build maven project")
+      }
+      stage("b"){
+        println("bbbb")
+      }
+      stage("c"){
+        println("cccc")
+      }
+      stage("d"){
+          println "hostname".execute().text   
+      }
   }
 }
